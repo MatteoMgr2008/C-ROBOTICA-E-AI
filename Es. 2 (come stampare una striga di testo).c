a4978e27e7ee713c1fbcf5a504720297f2aa299f
Matteo Magrino <145618533+MatteoMgr2008@users.noreply.github.com>
2024-04-29 02:00:06 +0200

    Add files via upload

---
+++ Es. 2 (come stampare una striga di testo).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+
+{
+// printf() displays the string inside quotation
+printf("Ciao come va la vita ?");
+return 0;
+}
---
+++ Es. 3 (come andare a capo in una stringa di testo).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+
+{
+// printf() displays the string inside quotation
+printf("Ciao come va \nla vita ?");
+return 0;
+}
